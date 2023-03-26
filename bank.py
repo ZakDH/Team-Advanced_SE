5b1a9c8b973713a9267c8bca3aeb9379e8913490
ZakDH <60523522+ZakDH@users.noreply.github.com>
2023-03-26 14:37:38 +0100

    added-error-handling

--- bank.py
+++ bank.py
@@ -3,19 +3,39 @@ from account import Account
 
 class Bank:    
     def __init__(self):
-        self.__accounts = {}
+        self.accounts = {}
 
     def createAccount(self, firstName, lastName):
         accountNumber = str(random.randint(100000, 999999))
-        self.__accounts[accountNumber] = Account(accountNumber)
+        account = Account(firstName, lastName, accountNumber)
+        self.accounts[accountNumber] = account
         print(f"Account created for {firstName} {lastName} with the account number {accountNumber}")
 
     def getAccount(self, accountNumber):
-        return self.__accounts.get(accountNumber)
+        return self.accounts.get(accountNumber)
 
     def viewDetails(self, accountNumber):
-        account = self.__accounts.get(accountNumber)
+        account = self.accounts.get(accountNumber)
         if account:
-            print(account)
+                print("--Account Details--")
+                print(f"\nFirst Name: {account.firstName}")
+                print(f"Last Name: {account.lastName}")
+                print(f"Account Number: {account.getAccountNumber()}")
+                print(f"Balance: {account.checkBalance():.2f}\n")
         else:
             print("Account not found.")
+    
+    def transfer(self, accountNumberFrom, accountNumberTo, amount):
+        accountFrom = self.getAccount(accountNumberFrom)
+        accountTo = self.getAccount(accountNumberTo)
+        if not accountFrom:
+            print(f"Account {accountNumberFrom} not found.")
+            return
+        if not accountTo:
+            print(f"Account {accountNumberTo} not found.")
+            return
+        try:
+            accountFrom.withdraw(amount, accountFrom)
+            accountTo.deposit(amount, accountTo)
+        except ValueError as e:
+            print(str(e))
