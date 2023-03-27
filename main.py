fc8231302d95fcad65a10add162f3d949a7af9f9
olalekanolanrewaju <78469928+olalekanolanrewaju@users.noreply.github.com>
2023-03-27 19:30:29 +0100

    Update main.py
    updated version with comments showing inheritance,encapsulation and polymorphism

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bank import Bank
 
 def main():
+    # Encapsulation: The Bank object is encapsulated within the main function
     bank = Bank()
+
     while True:
         print("What service do you wish to use?")
         print("1. Create an account")
@@ -20,6 +22,8 @@ def main():
         if choice == "1":
             firstName = input("What is your first name?: ")
             lastName = input("What is your last name?: ")
+            # Polymorphism: The createAccount method is called on the Bank object,
+            # which can create different types of accounts based on the arguments passed in
             bank.createAccount(firstName, lastName)
                 
         elif choice == "2":
@@ -30,6 +34,8 @@ def main():
                 while not depositAmount.isdigit():
                     print("Invalid amount... ")
                     depositAmount = input("Please enter deposit amount:" )
+                # Encapsulation: The deposit method is called on the Account object,
+                # which is encapsulated within the Bank object
                 account.deposit(float(depositAmount), accountNumber)
             else:
                 print("Account has not been found...")
@@ -43,6 +49,8 @@ def main():
                     print("Invalid amount... ")
                     withdrawAmount = input("Please enter withdrawal amount:" )
                 try:
+                    # Encapsulation: The withdraw method is called on the Account object,
+                    # which is encapsulated within the Bank object
                     account.withdraw(float(withdrawAmount), accountNumber)
                 except ValueError as e:
                     print(str(e))
@@ -57,12 +65,16 @@ def main():
                 print("Invalid amount... ")
                 transferAmount = input("Enter amount to transfer: ")
             try:
+                # Polymorphism: The transfer method is called on the Bank object,
+                # which can transfer funds between different types of accounts
                 bank.transfer(accountFrom, accountTo, float(transferAmount))
             except ValueError as e:
                 print(str(e))
         
         elif choice == "5":
             accountNumber = input("Enter account number: ")
+            # Inheritance: The viewDetails method is inherited from the Account class
+            # and is called on the appropriate Account object
             bank.viewDetails(accountNumber)
             
         elif choice == "6":
