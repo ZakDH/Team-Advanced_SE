2ce5376b9a17fa69418ca2d294b5d3e1357714e0
olalekanolanrewaju <78469928+olalekanolanrewaju@users.noreply.github.com>
2023-03-29 18:37:52 +0100

    Update test_.py
    Unittest for account.py class

--- test_.py
+++ test_.py
@@ -1,28 +1,43 @@
 import unittest
+from account import Account, SavingsAccount
 
-class TestBank(unittest.TestCase):
-    
+class TestAccount(unittest.TestCase):
+    def test_create_account(self):
+        account = Account("John", "Doe", "12345678", 1000.00)
+        self.assertEqual(account.firstName, "John")
+        self.assertEqual(account.lastName, "Doe")
+        self.assertEqual(account.accountNumber, "12345678")
+        self.assertEqual(account.balance, 1000.00)
+
     def test_deposit(self):
-        # Set up any necessary data or objects
-        account_balance = 100
-        
-        # Perform the action you want to test
-        deposit_amount = 50
-        account_balance += deposit_amount
-        
-        # Assert that the expected result has occurred
-        self.assertEqual(account_balance, 150)
-    
+        account = Account("John", "Doe", "12345678", 1000.00)
+        account.deposit(500.00, account.getAccountNumber())
+        self.assertEqual(account.balance, 1500.00)
+
     def test_withdraw(self):
-        # Set up any necessary data or objects
-        account_balance = 100
-        
-        # Perform the action you want to test
-        withdrawal_amount = 50
-        account_balance -= withdrawal_amount
-        
-        # Assert that the expected result has occurred
-        self.assertEqual(account_balance, 50)
+        account = Account("John", "Doe", "12345678", 1000.00)
+        account.withdraw(500.00, account.getAccountNumber())
+        self.assertEqual(account.balance, 500.00)
+
+    def test_insufficient_funds(self):
+        account = Account("John", "Doe", "12345678", 1000.00)
+        with self.assertRaises(ValueError):
+            account.withdraw(1500.00, account.getAccountNumber())
+
+class TestSavingsAccount(unittest.TestCase):
+    def test_create_savings_account(self):
+        account = SavingsAccount("John", "Doe", "12345678", 1000.00)
+        self.assertEqual(account.firstName, "John")
+        self.assertEqual(account.lastName, "Doe")
+        self.assertEqual(account.accountNumber, "12345678")
+        self.assertEqual(account.balance, 1000.00)
+def test_deposit_with_interest(self):
+    account = SavingsAccount("John", "Doe", "12345678", 1000.00)
+    account.deposit(500.00, account.getAccountNumber())
+    self.assertEqual(account.balance, 1505.00)
+    account.deposit(500.00, account.getAccountNumber())
+    self.assertEqual(account.balance, 1550.00)
+
 
 if __name__ == '__main__':
     unittest.main()
